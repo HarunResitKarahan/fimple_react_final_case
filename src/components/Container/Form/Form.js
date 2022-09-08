4fe0f39a0ba46861875fa5a0630b52471826ac36
Harun <harun_artin@hotmail.com>
2022-09-08 03:09:02 +0300

    .

--- src/components/Container/Form/Form.js
+++ src/components/Container/Form/Form.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useFormik } from 'formik';
+import "./Form.css"
 
 function Form() {
     const formik = useFormik({
@@ -15,46 +16,61 @@ function Form() {
   return (
     <>
         <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="firstName">Kredi Tutarı</label>
-            <input
-                id="firstName"
-                name="firstName"
-                type="text"
-                onChange={formik.handleChange}
-                value={formik.values.firstName}
-            />
-            <label htmlFor="lastName">Taksit Sayısı</label>
-            <input
-                id="lastName"
-                name="lastName"
-                type="text"
-                onChange={formik.handleChange}
-                value={formik.values.lastName}
-            />
+            <div className='form-items'>
+                <div className='label'>
+                    <label htmlFor="firstName">Kredi Tutarı: </label>
+                </div>
+                <div className='input'>
+                    <input
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.firstName}
+                    />
+                </div>
+                
+            </div>
+            <div className='form-items'>
+                <label htmlFor="lastName">Taksit Sayısı</label>
+                <input
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.lastName}
+                />
+            </div>
+            <div>
             <label htmlFor="email">Kâr oranı</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-            />
-            <label htmlFor="email">Taksit aralığı seçimi → aylık, haftalık, yıllık</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-            />
-            <label htmlFor="email">Vergi oranı → bsmv ve kkdf</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-            />
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Taksit aralığı seçimi → aylık, haftalık, yıllık</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Vergi oranı → bsmv ve kkdf</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+            </div>
             <button type="submit">Submit</button>
         </form>
     </>
