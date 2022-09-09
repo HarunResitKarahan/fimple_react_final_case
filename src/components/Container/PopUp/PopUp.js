b53632794a745be2e4e25672bc3ea77fc9e8559f
Harun <harun_artin@hotmail.com>
2022-09-09 15:41:22 +0300

    .

--- src/components/Container/PopUp/PopUp.js
+++ src/components/Container/PopUp/PopUp.js
@@ -8,6 +8,7 @@ import PopUpContext from "../../../context/PopUpContext"
 function PopUp() {
     const { formValues } = useContext(FormContext)
     const { setShowPupUp } = useContext(PopUpContext)
+    const headers = ['Taksit No', 'Taksit Tutarı', 'Ana Para', 'Kalan Ana Para', 'Kâr Tutarı', 'KKDF', 'BSMV']
     // console.log(formValues)
     // useEffect(() => {
     //     console.log(formValues)
@@ -15,7 +16,7 @@ function PopUp() {
     return (
         <div className='popUp'>
             <span className='popUpExit' onClick={() => { setShowPupUp(false) }}>
-                <IconContext.Provider value={{ color: "black", className: "global-class-name", size: "27px" }}>
+                <IconContext.Provider value={{ color: "#f77c34", className: "global-class-name", size: "27px" }}>
                     <div>
                         <BsXSquare />
                     </div>
@@ -24,6 +25,27 @@ function PopUp() {
             <div className='popUpContainer'>
                 <div className='containerHeader'>
                     <h1>Geri Ödeme Planı Tablosu</h1>
+                </div>
+                <div className='popUpTable'>
+                    <table>
+                        <tr>
+                            {headers.map((item, index) => (
+                                <th key={index}>{item}</th>
+                            ))}
+                        </tr>
+                        <tr>
+                            {[...Array(formValues.installmentCount)].map((x, i) =>
+                                <>
+                                    <td>{i + 1}</td>
+                                    <td>{formValues.creditAmount}</td>
+                                    <td>{formValues.interestRate}</td>
+                                    <td>{formValues.payment}</td>
+                                    <td>{formValues.taxBsmv}</td>
+                                    <td>{formValues.taxKkdf}</td>
+                                </>
+                            )}
+                        </tr>
+                    </table>
                 </div>
             </div>
             {JSON.stringify(formValues, null, 2)}
