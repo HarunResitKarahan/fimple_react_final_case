d95b27355d39409e5b4403b11090ddc6b93a24f1
Harun <harun_artin@hotmail.com>
2022-09-10 02:37:33 +0300

    .

--- src/components/Container/PopUp/PopUp.js
+++ src/components/Container/PopUp/PopUp.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import "./PopUp.css"
 import FormContext from '../../../context/FormContext'
 import { BsXSquare } from "react-icons/bs";
@@ -10,18 +10,37 @@ function PopUp() {
     const { setShowPupUp } = useContext(PopUpContext)
     const headers = ['Taksit No', 'Taksit Tutarı', 'Ana Para', 'Kalan Ana Para', 'Kâr Tutarı', 'KKDF', 'BSMV']
     const taxBsmv = (formValues.taxBsmv * formValues.creditAmount) / 100
-    const taxKkdf = formValues.taxKkdf * formValues.creditAmount / 100
+    const taxKkdf = (formValues.taxKkdf * formValues.creditAmount) / 100
     const Rate = (formValues.interestRate / 100) + (formValues.taxBsmv / 100) + (formValues.taxKkdf / 100)
     const Nper = formValues.installmentCount
-    const Pv = formValues.creditAmount
+    let Pv = formValues.creditAmount
+    const interestRate = (formValues.interestRate / 100)
     const payment = Pv * ((Rate * ((1 + Rate) ** Nper)) / ((((1 + Rate) ** Nper) - 1)))
     const profit = (formValues.creditAmount * (formValues.interestRate / 100))
-    const mainMoney = payment - taxBsmv - taxKkdf - profit
-    const [unpaidMainMoney, setUnpaidMainMoney] = useState(Pv - mainMoney)
+    const tableValues = {
+        'mainMoney': [],
+        'unpaidMainMoney': [],
+        'profit': [],
+        'taxKkdf': [],
+        'taxBsmv': []
+    }
+    // console.log(payment)
+    // let tempAmount = Pv
+    for (let i = 0; i < formValues.installmentCount; i++) {
+        tableValues.mainMoney.push(Number(payment) - Number(taxBsmv) - Number(taxKkdf) - Number(profit))
+        tableValues['unpaidMainMoney'].push(Number(Pv) - (Number(payment) - Number(taxBsmv) - Number(taxKkdf) - Number(profit)))
+        tableValues['profit'].push((Pv * interestRate))
+        tableValues['taxKkdf'].push(taxKkdf)
+        tableValues['taxBsmv'].push(taxBsmv)
+        Pv = Number(tableValues.unpaidMainMoney[i])
+    }
+    console.log(tableValues)
+    // const [unpaidMainMoney, setUnpaidMainMoney] = useState(Number(Pv) - Number(mainMoney))
+    // console.log(Pv - mainMoney)
     // console.log(formValues)
-    // useEffect(() => {
-    //     console.log(formValues)
-    // }, [formValues])
+    useEffect(() => {
+        // console.log(tableValues)
+    }, [])
     return (
         <div className='popUp'>
             <span className='popUpExit' onClick={() => { setShowPupUp(false) }}>
@@ -45,19 +64,19 @@ function PopUp() {
                             </tr>
                         </thead>
 
-                        <tbody>
+                        {/* <tbody>
                             {[...Array(formValues.installmentCount)].map((x, i) =>
                                 <tr key={i}>
                                     <td>{i + 1}</td>
                                     <td>{Number(payment.toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
-                                    <td>{Number(mainMoney.toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
-                                    <td>{Number(Number(unpaidMainMoney).toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
-                                    <td>{Number(profit.toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
-                                    <td>{Number((taxKkdf.toFixed(2))).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
-                                    <td>{Number((taxBsmv.toFixed(2))).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
+                                    <td>{Number(tableValues.mainMoney[i].toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
+                                    <td>{Number(tableValues.unpaidMainMoney[i].toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
+                                    <td>{Number(tableValues.profit[i].toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
+                                    <td>{Number(tableValues.taxKkdf[i].toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
+                                    <td>{Number(tableValues.taxBsmv[i].toFixed(2)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ₺</td>
                                 </tr>
                             )}
-                        </tbody>
+                        </tbody> */}
 
                     </table>
                 </div>
